ff0bb86306e72c06f8f33d9a9fce079593dcee44
kalung2002 <kalung2002@hotmail.com>
2021-08-28 06:32:38 +0800

    Test all timing on UNO and compacted the code

--- connect.cpp
+++ connect.cpp
@@ -8,6 +8,7 @@
 
 #include <string>
 #include <vector>
+#include <deque>
 
 using namespace std;
 void pause()
@@ -30,60 +31,46 @@ void PrintCommState(DCB dcb)
            dcb.StopBits);
 }
 
-bool SelectComPort(vector<pair<string, string>> &list) //added function to find the present serial
+bool findAndConnect(deque<string> &raw_data)
 {
+    vector<pair<string, string>> list;
     char lpTargetPath[5000]; // buffer to store the path of the COMPORTS
     bool gotPort = false;    // in case the port is not found
-    list.clear();
-    for (int i = 0; i < 255; i++) // checking ports from COM0 to COM255
-    {
-        // converting to COM0, COM1, COM2
-        string str = "COM" + to_string(i);
-        LPCSTR lstr = str.c_str();
-        //DWORD QueryDosDeviceW(
-        //   LPCWSTR lpDeviceName,
-        //   LPWSTR  lpTargetPath,
-        //   DWORD   ucchMax
-        //   );
-        DWORD test = QueryDosDevice(lstr, lpTargetPath, 5000);
 
-        // Test the return value and error if any
-        if (test != 0) //QueryDosDevice returns zero if it didn't find an object
-        {
-            // pair<string, string> buffer(lstr, lpTargetPath);
-            list.push_back(make_pair(lstr, lpTargetPath));
-            gotPort = true;
-        }
+    string sinput; //string input from user
+    int input = 0; //sinput turned into int
 
-        if (::GetLastError() == ERROR_INSUFFICIENT_BUFFER)
-        {
-        }
-    }
-    return gotPort;
-}
-int listAndChoose(vector<pair<string, string>> &list)
-{
-    int in = -1;
-    bool mode = 0;
+    string raw_data_s;
+    HANDLE m_hCommPort = INVALID_HANDLE_VALUE;
     do
     {
-        if (SelectComPort(list)) //if got port
+        for (int i = 0; i < 255; i++) // checking ports from COM0 to COM255
         {
-            mode = 1;
-            cout << "Select connection from list\n================" << endl;
-            for (auto it = list.begin(); it != list.end(); ++it)
+            // converting to COM0, COM1, COM2
+            string str = "COM" + to_string(i);
+            LPCSTR lstr = str.c_str();
+            //DWORD QueryDosDeviceW(
+            //   LPCWSTR lpDeviceName,
+            //   LPWSTR  lpTargetPath,
+            //   DWORD   ucchMax
+            //   );
+            DWORD test = QueryDosDevice(lstr, lpTargetPath, 5000);
+
+            // Test the return value and error if any
+            if (test != 0) //QueryDosDevice returns zero if it didn't find an object
             {
-                cout << it - list.begin() << " "
-                     << " |Port:" << it->first
-                     << " |Location:" << it->second
-                     << endl;
+                // pair<string, string> buffer(lstr, lpTargetPath);
+                list.push_back(make_pair(lstr, lpTargetPath));
+                gotPort = true;
             }
-            cout << "================" << endl;
+
+            // if (::GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+            // {
+            // }
         }
-        else
+        if (!gotPort)
         {
             cout << "waiting device to connect" << flush;
-            Sleep(100);
             for (int i = 0; i < 10; i++)
             {
                 Sleep(100);
@@ -92,42 +79,51 @@ int listAndChoose(vector<pair<string, string>> &list)
             cout << endl;
             clear();
         }
+        else
+        {
+            clear();
+        }
+    } while (!gotPort);
 
-    } while (!mode);
-    if (mode)
+    if (gotPort)
     {
-        string input;
+        cout << "Select connection from list\n================" << endl;
+        for (auto it = list.begin(); it != list.end(); ++it)
+        {
+            cout << it - list.begin() << " "
+                 << " |Port:" << it->first
+                 << " |Location:" << it->second
+                 << endl;
+        }
+        cout << "================" << endl;
         cout << "Enter what device to connect: ";
-        cin >> input;
-        in = stoi(input);
-        cout << flush << endl;
-        clear();
+        cin >> sinput;
+        input = list.size() + 1; //that way input must go through stoi
+        input = stoi(sinput);    //sinput convert into int
+        cout << flush << endl;   //flush just to be safe
+        if (input < list.size())
+        {
+            string deviceName = list[input].first;
+            if (list[input].first.size() > 4)
+            {
+                // cout << list[input].first << "|||" << list[input].first.size() << endl;
+                deviceName = "\\\\.\\" + deviceName;
+                // cout << deviceName << endl;
+            }
+            m_hCommPort = ::CreateFile(
+                deviceName.c_str(),
+                GENERIC_READ | GENERIC_WRITE, //access ( read and write)
+                0,                            //(share) 0:cannot share the COM port
+                0,                            //security  (None)
+                OPEN_EXISTING,                // creation : open_existing
+                0,
+                // FILE_FLAG_OVERLAPPED,         // we want overlapped operation
+                0 // no templates file for COM port...
+            );
+        }
     }
-    return in;
-}
-bool connectToPort(vector<pair<string, string>> &list, int i)
-{
-    if (i >= list.size())
-    {
-        return false;
-    }
-    string deviceName = list[i].first;
-    if (list[i].first.size() > 4)
-    {
-        cout << list[i].first << "|||" << list[i].first.size() << endl;
-        deviceName = "\\\\.\\" + deviceName;
-        cout << deviceName << endl;
-    }
-    HANDLE m_hCommPort = ::CreateFile(
-        deviceName.c_str(),
-        GENERIC_READ | GENERIC_WRITE, //access ( read and write)
-        0,                            //(share) 0:cannot share the COM port
-        0,                            //security  (None)
-        OPEN_EXISTING,                // creation : open_existing
-        0,
-        // FILE_FLAG_OVERLAPPED,         // we want overlapped operation
-        0 // no templates file for COM port...
-    );
+    clear();
+
     if (m_hCommPort == INVALID_HANDLE_VALUE)
     {
         cout << "ERROR" << endl;
@@ -136,49 +132,61 @@ bool connectToPort(vector<pair<string, string>> &list, int i)
     else
     {
         bool fSuccess = false;
-        cout << "EEEEEEEEEEEEEEEEEEEEEE" << endl;
         DCB dcb;
         SecureZeroMemory(&dcb, sizeof(DCB));
         dcb.DCBlength = sizeof(DCB);
-
         //  Fill in some DCB values and set the com state:
         //  115,200 bps, 8 data bits, no parity, and 1 stop bit.
         dcb.BaudRate = CBR_115200; //  baud rate
         dcb.ByteSize = 8;          //  data size, xmit and rcv
         dcb.Parity = NOPARITY;     //  parity bit
         dcb.StopBits = ONESTOPBIT; //  stop bit
-        fSuccess = GetCommState(m_hCommPort, &dcb);
 
+        fSuccess = GetCommState(m_hCommPort, &dcb);
         while (fSuccess)
         {
-            DWORD bytesRead, eventMask;
+            COMSTAT comStatus;
+            DWORD bytesRead, error;
             char data;
             bool result = false;
-            result = ReadFile(m_hCommPort, &data, sizeof(data), &bytesRead, NULL);
-            if (data == '\t')
+            ClearCommError(m_hCommPort, &error, &comStatus);
+            if (comStatus.cbInQue)
             {
-                cout << endl
-                     << endl;
-            }
-            else
-            {
-                printf("%c", data);
+                result = ReadFile(m_hCommPort, &data, sizeof(data), &bytesRead, NULL);
+                if (data == '\n')
+                {
+                    raw_data.push_back(raw_data_s);
+                    // cout << raw_data_s << endl;
+
+                    raw_data_s.clear();
+                    if (raw_data.size() > 256)
+                    {
+                        raw_data.pop_front();
+                        return false;
+                    }
+                }
+                else
+                {
+                    raw_data_s.push_back(data);
+                }
             }
         }
         return true;
     }
     return false;
 }
+
 main()
 {
     clear();
-    vector<pair<string, string>> list;
-    int i = listAndChoose(list);
-    if (!list.empty() && i >= 0 && i < list.size())
+    deque<string> raw_data;
+
+    findAndConnect(raw_data);
+    for (int i = 0; i < raw_data.size(); i++)
     {
-        cout << "connecting to port " << i << "|" << list[i].first << endl;
+        cout << raw_data[i] << endl;
     }
-    connectToPort(list, i);
+
     printf("success\n");
     pause();
     return 0;
--- sketch_jul18a/sketch_jul18a.ino
+++ sketch_jul18a/sketch_jul18a.ino
@@ -1,63 +1,143 @@
 #include <EMGFilters.h>
+#include <base64.hpp>
+
 #define _DEBUG      0
 #define CALIBRATE 1
 //int baseline = 200;
-long dtime=millis();
 
-long maxtime=millis();
-int local_max=0;
-EMGFilters myFilter;
+unsigned int dtime = micros();
+unsigned int tdelay;
 
+//char char_set[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" ;
+char data_before[18];//no need \0 since it is temp
+char data_pack[25];
+//char data_pack[33];
 
+unsigned int TIM0;//opt time  //32
+//
+unsigned short EMG0;          //16
+unsigned short EMG1;          //16
+//
+unsigned short EMG2;          //16
+unsigned short EMG3;          //16
+//
 
+unsigned short IMUX;            //8
+unsigned short IMUY;            //8
+unsigned short IMUZ;            //8
+//
+unsigned short IMURY;           //8
+unsigned short IMURP;           //8
+unsigned short IMURR;           //8
+////////////////////
+//unsigned short IMUX;
+//unsigned short IMUY;
+//unsigned short IMUZ;
+////
+//unsigned short IMURY;
+//unsigned short IMURP;
+//unsigned short IMURR;
+/////////////////////
+int data0;
+int data1;
+int data2;
+int data3;
+
+EMGFilters Filter0;
+EMGFilters Filter1;
+EMGFilters Filter2;
+EMGFilters Filter3;
 SAMPLE_FREQUENCY sampleRate = SAMPLE_FREQ_1000HZ;
 NOTCH_FREQUENCY humFreq = NOTCH_FREQ_50HZ;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-   myFilter.init(sampleRate, humFreq, false, true, true);
-//   myFilter.init(sampleRate, humFreq, true, true, true);
+  //  pinMode(13, OUTPUT);
+  
+   Filter0.init(sampleRate, humFreq, false, true, true);
+   Filter1.init(sampleRate, humFreq, false, true, true);
+   Filter2.init(sampleRate, humFreq, false, true, true);
+   Filter3.init(sampleRate, humFreq, false, true, true);
+  //   myFilter.init(sampleRate, humFreq, true, true, true);
 
+  
   Serial.begin(115200);
-//Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  dtime=millis();
-  if(dtime%500==0){
-    digitalWrite(13, !digitalRead(13));
-   }
+//  TIM0=micros();
+//  dtime = micros();
+  data0 =analogRead(A0);
+  data0 =analogRead(A0);
+  data0 =analogRead(A0);
+  data0 =analogRead(A0);
+//  tdelay =micros()-dtime;
+//  dtime = micros();
+  EMG0 = Filter0.update(data0);  
+  EMG0 = Filter0.update(data0);
+  EMG0 = Filter0.update(data0);
+  EMG0 = Filter0.update(data0);
+//  tdelay =micros()-dtime;
+  if(EMG0<0){EMG0=abs(EMG0);}
+  if(EMG0<0){EMG0=abs(EMG0);}
+  if(EMG0<0){EMG0=abs(EMG0);}
+  if(EMG0<0){EMG0=abs(EMG0);}
+//tdelay =micros()-dtime;
+//dtime = micros();
+IMURR=EMG0;
+EMG1=EMG2=EMG3=IMUX=IMUY=IMUZ=IMURY=IMURP=IMURR;
+  //5*int with 4*: as seperation
+  //192 bit in 6bit base64
+  data_before[0]=(TIM0& 0b11111111000000000000000000000000)>>24;
+  data_before[1]=(TIM0& 0b00000000111111110000000000000000)>>16;
+
+  data_before[2]=highByte(TIM0);
+  data_before[3]=lowByte (TIM0);
+
+  data_before[4]=highByte(EMG0);
+  data_before[5]=lowByte (EMG0);
+
+  data_before[6]=highByte(EMG1);
+  data_before[7]=lowByte (EMG1);
+
+  data_before[8]=highByte(EMG2);
+  data_before[9]=lowByte (EMG2);
+
+  data_before[10]=highByte(EMG3);
+  data_before[11]=lowByte (EMG3);
   
-//  delay(200);  // Wait for 1000 millisecond(s)
-int data =analogRead(A0);
-int fdata = myFilter.update(data);
-int sqdata=sq(fdata);
+  data_before[12]=lowByte (IMUX);
+  data_before[13]=lowByte (IMUY);
+  data_before[14]=lowByte (IMUZ);
+  
+  data_before[15]=lowByte (IMURY);
+  data_before[16]=lowByte (IMURP);
+  data_before[17]=lowByte (IMURR);
+
+  encode_base64(data_before,18,data_pack);
+  
+//
+//  data_pack[0]=highByte();
+//  data_pack[1]=lowByte();
+//  
+//  data_pack[0]=highByte();
+//  data_pack[1]=lowByte();
+//
+//  data_pack[0]=highByte();
+//  data_pack[1]=lowByte();
 
 
-if(sqdata>local_max||abs(maxtime-dtime)>200){
-    local_max=sqdata;
-    maxtime=dtime;
-  }
 
+  
+//  tdelay =micros()-dtime;
+//  dtime = micros();
+  dtime = micros();  
+  Serial.print(tdelay);
+//  Serial.print(' ');
+  Serial.println(data_pack);
+  
+  tdelay =micros()-dtime;
 
-
-
-Serial.print(local_max);
- Serial.print("\t");
-  Serial.print(sqdata);
-  Serial.print("\t");
-  Serial.print(1000);
-  Serial.print("\t");
-  Serial.print(1000);
-  Serial.print("\t");
-  Serial.print(1000);
-  Serial.print("\t");
-  Serial.print(1000);
-  Serial.print("\t");
-  Serial.print(1000);
-  Serial.print("\t");
-  Serial.println(2000);
-//  Serial.println(data);
 }
