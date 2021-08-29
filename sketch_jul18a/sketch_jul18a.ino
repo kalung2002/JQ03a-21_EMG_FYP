c608e2ab795668323f8bc94e902d8263da632a40
kalung2002 <kalung2002@hotmail.com>
2021-08-30 05:52:33 +0800

    thread to decode done

--- sketch_jul18a/sketch_jul18a.ino
+++ sketch_jul18a/sketch_jul18a.ino
@@ -12,28 +12,28 @@
 #include <base64.hpp>
 
 unsigned int duration = micros();
-
+unsigned int counter = 0;
 //char char_set[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" ;
-char data_before[18];//no need \0 since it is temp
+char data_before[18]; //no need \0 since it is temp
 char data_pack[25];
 //char data_pack[33];
 
-unsigned int TIM0;//opt time  //32
+unsigned long TIM0; //opt time  //32
 //
-unsigned short EMG0;          //16
-unsigned short EMG1;          //16
+unsigned short EMG0; //16
+unsigned short EMG1; //16
 //
-unsigned short EMG2;          //16
-unsigned short EMG3;          //16
+unsigned short EMG2; //16
+unsigned short EMG3; //16
 //
 
-unsigned short IMUX;            //8
-unsigned short IMUY;            //8
-unsigned short IMUZ;            //8
+unsigned short IMUX; //8
+unsigned short IMUY; //8
+unsigned short IMUZ; //8
 //
-unsigned short IMURY;           //8
-unsigned short IMURP;           //8
-unsigned short IMURR;           //8
+unsigned short IMURY; //8
+unsigned short IMURP; //8
+unsigned short IMURR; //8
 ////////////////////
 //unsigned short IMUX;
 //unsigned short IMUY;
@@ -47,69 +47,84 @@ unsigned short IMURR;           //8
 //SAMPLE_FREQUENCY sampleRate = SAMPLE_FREQ_1000HZ;
 //NOTCH_FREQUENCY humFreq = NOTCH_FREQ_50HZ;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  //  pinMode(13, OUTPUT);
-  
+  pinMode(13, OUTPUT);
+
   //   myFilter.init(sampleRate, humFreq, true, true, true);
 
-//  Serial.begin(115200);
-// set prescale to 16
-  sbi(ADCSRA,ADPS2);
-  cbi(ADCSRA,ADPS1);
-  cbi(ADCSRA,ADPS0);
+  //  Serial.begin(115200);
+  // set prescale to 16
+  sbi(ADCSRA, ADPS2);
+  cbi(ADCSRA, ADPS1);
+  cbi(ADCSRA, ADPS0);
 
-//  Serial.begin(256000);
+  //  Serial.begin(256000);
   Serial.begin(500000);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  TIM0 = micros();
-  
-  EMG0 =analogRead(A0);
-  EMG1 =analogRead(A1);
-  EMG2 =analogRead(A2);
-  EMG3 =analogRead(A3);
 
-  EMG3 =analogRead(A4);
-  EMG3 =analogRead(A5);
-  IMURP=IMURY=IMURR=IMUX=IMUY=IMUZ=EMG3=EMG2=EMG1=EMG0;
+  TIM0 = micros();
+
+  EMG0 = analogRead(A0);
+  EMG1 = analogRead(A1);
+  EMG2 = analogRead(A2);
+  EMG3 = analogRead(A3);
+
+  EMG3 = analogRead(A4);
+  EMG3 = analogRead(A5);
+  IMURP = IMURY = IMURR = IMUX = IMUY = IMUZ = EMG3 = EMG2 = EMG1 = EMG0;
 
   //144 bit in 6bit base64
-  data_before[0]=(TIM0& 0b11111111000000000000000000000000)>>24;
-  data_before[1]=(TIM0& 0b00000000111111110000000000000000)>>16;
+  data_before[0] = ((TIM0 & 0b11111111000000000000000000000000)>>24);
+  data_before[1] = ((TIM0 & 0b111111110000000000000000)>>16);
+  data_before[2] = highByte(TIM0);
+  data_before[3] = lowByte(TIM0);
 
-  data_before[2]=highByte(TIM0);
-  data_before[3]=lowByte (TIM0);
+  data_before[4] = highByte(EMG0);
+  data_before[5] = lowByte(EMG0);
 
-  data_before[4]=highByte(EMG0);
-  data_before[5]=lowByte (EMG0);
+  data_before[6] = highByte(EMG1);
+  data_before[7] = lowByte(EMG1);
 
-  data_before[6]=highByte(EMG1);
-  data_before[7]=lowByte (EMG1);
+  data_before[8] = highByte(EMG2);
+  data_before[9] = lowByte(EMG2);
 
-  data_before[8]=highByte(EMG2);
-  data_before[9]=lowByte (EMG2);
+  data_before[10] = highByte(EMG3);
+  data_before[11] = lowByte(EMG3);
 
-  data_before[10]=highByte(EMG3);
-  data_before[11]=lowByte (EMG3);
-  
-  data_before[12]=lowByte (IMUX);
-  data_before[13]=lowByte (IMUY);
-  data_before[14]=lowByte (IMUZ);
-  
-  data_before[15]=lowByte (IMURY);
-  data_before[16]=lowByte (IMURP);
-  data_before[17]=lowByte (IMURR);
+  data_before[12] = lowByte(IMUX);
+  data_before[13] = lowByte(IMUY);
+  data_before[14] = lowByte(IMUZ);
 
-  encode_base64(data_before,18,data_pack);
-  
-  Serial.print(duration);
+  data_before[15] = lowByte(IMURY);
+  data_before[16] = lowByte(IMURP);
+  data_before[17] = lowByte(IMURR);
+
+  encode_base64(data_before, 18, data_pack);
+
+//    Serial.print(micros());
+//    Serial.print(TIM0);
   Serial.println(data_pack);
-  duration = micros()-TIM0;
-  if(duration<980){
-   delayMicroseconds(1000-duration);
-  }
+  duration = micros() - TIM0;
 
+  if (duration < 900)
+  {
+    counter++;
+    counter = counter % 500;
+    if (counter > 250)
+    {
+
+      digitalWrite(13, HIGH);
+    }
+    else
+    {
+      digitalWrite(13, LOW);
+    }
+
+  }
 }
